982ffd51300c41f188e88111c16ece8540527275
grupoa32026 <daniel.garcia166@educa.madrid.org>
2026-01-14 09:49:32 +0100

    Empezar

---
+++ miPrimeraAPI/api/web/controlador_comentarios.py
@@ -0,0 +1,43 @@
+from bd import obtener_conexion
+import sys
+import datetime as dt
+
+
+def convertir_comentario_a_json(comentario):
+    d = {}
+    d['id'] = comentario[0]
+    d['usuario'] = comentario[1]
+    d['descripcion'] = comentario[2]
+    return d
+
+def insertar_comentario(usuario, descripcion):
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            cursor.execute("INSERT INTO comentarios(usuario, descripcion) VALUES ('"+ usuario +"','" + descripcion + "')")
+            conexion.commit()
+        conexion.close()
+        ret={"status": "OK" }
+        code=200
+    except:
+        ret={"status": "ERROR" }
+        print("Excepcion al insertar un comentario", flush=True)
+        code=500   
+    return ret,code
+
+def obtener_comentarios():
+    comentariosjson=[]
+    try:
+        conexion = obtener_conexion()
+        with conexion.cursor() as cursor:
+            cursor.execute("SELECT id, usuario, descripcion FROM comentarios")
+            comentarios = cursor.fetchall()
+            if comentarios:
+                for comentario in comentarios:
+                    comentariosjson.append(convertir_comentario_a_json(comentario))
+        conexion.close()
+        code=200
+    except:
+        print("Excepcion al consultar todas los comentarios", flush=True)
+        code=500
+    return comentariosjson,code
